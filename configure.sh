3ceaa5f57c21271f7d2077caebbb5fbeb59bbfde
Miguel Morillo <miguelmwilliams@gmail.com>
2022-11-24 19:42:36 +0100

    improvement(php): create docker-compose.yml to manage php.
    Added two version to php, development and production.

--- configure.sh
+++ configure.sh
@@ -2,3 +2,13 @@ if ! test -f ".env"; then
   echo "Copy .env"
   cp .env.example .env
 fi
+
+if ! test -f "php/development/.env"; then
+  echo "Copy php/development/.env"
+  cp php/development/.env.example php/development/.env
+fi
+
+if ! test -f "php/production/.env"; then
+  echo "Copy php/production/.env"
+  cp php/production/.env.example php/production/.env
+fi
