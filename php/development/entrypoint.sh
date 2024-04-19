cc4991e07113982301550a4299256c316112f7f2
Miguel Morillo <miguelmwilliams@gmail.com>
2024-04-19 17:57:39 +0200

    improvement(php): ensure vendor not exist to execute compose install and export .env.build if exist

--- php/development/entrypoint.sh
+++ php/development/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-composer install --no-interaction --optimize-autoloader
+if [ -f ".env.build" ]; then
+    export "$(grep -v '^#' .env.build | xargs)"
+fi
+
+if [ ! -d "vendor" ]; then
+    composer install --no-interaction --optimize-autoloader
+fi
+
 php-fpm -F -R
