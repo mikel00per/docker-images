f2acfc5b30f870dfa801fd6c4fb9d5eaacc80f70
Miguel Morillo <miguelmwilliams@gmail.com>
2024-04-26 19:00:41 +0200

    fix(php-cli): cli container death

--- php-cli/development/entrypoint.sh
+++ php-cli/development/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -f ".env.build" ]; then
     export $(grep -v '^#' .env.build | tr -d '"' | tr -d "'" | xargs -0)
 fi
@@ -7,3 +9,10 @@ fi
 if [ ! -d "vendor" ]; then
     composer install --no-interaction --optimize-autoloader
 fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php "$@"
+fi
+
+exec "$@"
